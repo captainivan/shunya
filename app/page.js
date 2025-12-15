0ac103a1921af3a1058ca8f96cee06e2743e440f
ivancap7777 <ivancap7777@gmail.com>
2025-12-15 16:24:40 +0000

    god update

--- app/page.js
+++ app/page.js
@@ -2,7 +2,8 @@
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>Shunya</h1>
       <iframe
         src="https://getdevcard.vercel.app/devcard/naVbLHDMIPhzW7gkllr0"
         style={{background:"transparent",overflow:"hidden"}}
